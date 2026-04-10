95e20a28b1b34f0e4462bd293957aab19b9ab564
haykel <haykel.j@gmail.com>
2026-04-10 15:17:18 +0200

    fix: remove import.meta.env from keycloak.ts

--- frontend/src/keycloak.ts
+++ frontend/src/keycloak.ts
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: import.meta.env.VITE_KEYCLOAK_URL || "http://localhost:8180",
+  url: "http://localhost:8180",
   realm: "investissement",
   clientId: "frontend",
 });
